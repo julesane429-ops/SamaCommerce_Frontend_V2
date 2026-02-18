cbda9ed0c1d2c2925cad0f7ae86c61b1b99c0602
julesane429-ops <julesane429@gmail.com>
2026-02-18 15:35:08 +0000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,7 +2,6 @@ import { appData,chartVentesByDay, chartTopProduits, chartPaiements, chartStocks
 import { afficherRapports, updateStats, afficherStatsCredits  } from "./rapports.js";
 import { afficherInventaire, setupSearchInputs, remplirSelectProduitsCredit } from "./inventaire.js";
 import { updateCharts, renderVentesChart, initCreditChart } from "./charts.js";
-import { authfetch, postCategoryServer, postProductServer, syncFromServer } from "./api.js";
 import { getCurrentUserId, logout } from "./auth.js";
 import { selectEmoji, supprimerCategorie, ajouterCategorie,remplirSelectCategories, afficherFiltresCategories } from "./categories.js";
 import { renderCreditsHistory,marquerCreditPaye, confirmerRemboursement, remplirProduitsCredit } from "./credits.js";
